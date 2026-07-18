44ad6f1728aa9d76b001145e8a962071b5b90db2
Curtiss Howard <curtiss.howard@analyst1.com>
2026-07-18 08:02:22 -0400

    VPN fix.

--- src/nix/host/physical/work/zsh.nix
+++ src/nix/host/physical/work/zsh.nix
@@ -2,8 +2,8 @@
 {
   programs.zsh.initContent = ''
     wg-start-stop() {
-      if sudo wg show wg0 > /dev/null 2>&1; then
-        sudo wg-quick down wg0
+      if sudo test -f /var/run/wireguard/wg0.name; then
+        sudo wg-quick down /etc/wireguard/wg0.conf
       else
         sudo install -d -m 700 /etc/wireguard
         sudo sh -c 'umask 077; {
@@ -17,7 +17,7 @@
           printf "%s\n" "AllowedIPs = 10.0.0.0/8"
           printf "%s\n" "PersistentKeepalive = 30"
         } > /etc/wireguard/wg0.conf'
-        sudo wg-quick up wg0
+        sudo wg-quick up /etc/wireguard/wg0.conf
       fi
     }
   '';
