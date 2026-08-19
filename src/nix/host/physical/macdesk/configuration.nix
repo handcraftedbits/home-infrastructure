ab2a488f922c0b93dec708a1dbc718e4b6d0d4fe
Curtiss Howard <opensource@handcraftedbits.com>
2026-08-19 15:56:43 -0400

    Get set back up for Maven publishing.

--- src/nix/host/physical/macdesk/configuration.nix
+++ src/nix/host/physical/macdesk/configuration.nix
@@ -8,7 +8,28 @@
   home-manager.users.${vars.user.username} = { ... }: {
     imports = [
       ./aerospace.nix
+      ./gnupg.nix
+      ./maven.nix
     ];
+  };
+
+  # Secrets
+  age.secrets = {
+    "gpg/privateKey" = {
+      file = ../../../module/secret/gpg/privateKey.age;
+      mode = "0600";
+      owner = vars.user.username;
+    };
+    "sonatype/password" = {
+      file = ../../../module/secret/sonatype/password.age;
+      mode = "0400";
+      owner = vars.user.username;
+    };
+    "sonatype/username" = {
+      file = ../../../module/secret/sonatype/username.age;
+      mode = "0400";
+      owner = vars.user.username;
+    };
   };
 
   environment.systemPackages = with pkgs; [
